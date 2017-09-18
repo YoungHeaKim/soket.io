fd01eb17d4fd5c5e3ba7fe4fae09fe97013afe67
YoungHeaKim <dydgo9507@gmail.com>
2017-09-18 16:56:33 +0900

    채팅이 보내졌을 때 확인

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@ function appendText(messageListEl, text) {
   const messageEl = document.createElement('p')
   messageEl.textContent = text
   messageEl.classList.add('message')
-  messageEl.classList.add('new')
+  messageEl.classList.add('new') // 낙관적 업데이트를 하고 있다.
   messageListEl.insertBefore(messageEl, messageListEl.firstChild)
   return messageEl
 }
@@ -33,7 +33,11 @@ document.addEventListener('DOMContentLoaded', e => {
     e.preventDefault()
     // chat.pug에서 input#message.form-control(type="text", required)부분에 들어오는 값을 바로 불러온다.
     const message = formEl.elements.message.value
-    socket.emit('new chat', {message})
+    const messageEl = appendText(messageListEl, formatMessage({username, message}))
+    socket.emit('new chat', {message}, data => {
+      //메세지 전송이 잘 되었다는 표시를 해주면 됨(자기만 new가 지워진다.)
+      messageEl.classList.remove('new')
+    })
     // form에 있는 값을 초기화 시킨다.
     formEl.reset()
   })
@@ -41,7 +45,11 @@ document.addEventListener('DOMContentLoaded', e => {
 
   // (chat) 채팅 메시지가 올 때마다 출력
   socket.on('chat', data => {
-    appendText(messageListEl, `${data.username}: ${data.message}`)
+    const messageEl = appendText(messageListEl, formatMessage(data))
+    // 상대편의 체팅에서도 스르륵 나타나는 효과를 준다.
+    setTimeout(() => {
+      messageEl.classList.remove('new')
+    })
   })
 
   // (user connected) 새 사용자가 접속한 사실을 출력
